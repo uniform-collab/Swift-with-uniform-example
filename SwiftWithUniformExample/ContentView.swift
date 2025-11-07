9b8325ae502287400d1580fb2d8919aff39f36ff
nikitam <nikitam@uniform.dev>
2025-11-06 19:00:52 -0500

    Initial Commit

---
+++ SwiftWithUniformExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftWithUniformExample
+//
+//  Created by uniform on 06-11-2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
